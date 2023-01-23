73e834c8c5fb4c77d67b07c926bf50a3e8b11d5e
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-01-23 20:15:47 +0530

    Create 06_ProblemStatement.py

---
+++ 04/06_ProblemStatement.py
@@ -0,0 +1,15 @@
+TRAINING_DATA = [
+ 
+  (“Linkedin is the most powerful place for networking.”, 
+  
+   {'entities': [(_________, _________, 'WEBSITE')]}),
+ 
+  (“Ram has a very attractive profile on Linkedin.”, 
+  
+   {'entities': [(_________,_________, 'PERSON'), (_________, _________, 'WEBSITE')]}),
+ 
+  (“Ram recently published a Machine Learning course on Linkedin Learning.”, 
+  
+   {'entities': [(_________, _________, 'WEBSITE'), (_________,_________, ‘PERSON’)]})
+
+]
