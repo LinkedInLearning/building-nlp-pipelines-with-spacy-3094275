ed2c48d5ab8b2e09acf782f42c27e39962e36b8d
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-01-23 19:23:10 +0530

    Create 05_ProblemStatement.py

---
+++ 01/05_ProblemStatement.py
@@ -0,0 +1,26 @@
+# Part - 1
+
+text = “Ram goes to college everyday by car.”
+# Importing spacy and getting all attributes.
+
+nlp = _________
+doc = _________
+
+for token in doc:
+  token_text = _________ # Getting the corresponding text
+  token_dep = _________ # Getting the Dependency label
+  token_pos = _________ # Getting the POS Tag
+  print(token_text, token_dep, token_pos)
+
+
+# Part - 2
+
+text = “Ram goes to college everyday by car.”
+# Importing spacy and getting all attributes.
+
+nlp = _________
+doc = _________
+
+for _________ in _________:
+  # Printing the entity text and label
+  print(_________, _________)
