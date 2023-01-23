33633431962b67505b5f8cb7c09ada441c7f9ed2
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-01-23 19:37:34 +0530

    Create 04_Solution.py

---
+++ 02/04_Solution.py
@@ -0,0 +1,11 @@
+# Importing Phrase Matcher & initialise it.
+from spacy.matcher import PhraseMatcher
+matcher = PhraseMatcher(nlp.vocab)
+
+# Creating pattern doc objects
+patterns = list(nlp.pipe(COUNTRIES))
+matcher.add('COUNTRY', None, *patterns) # Adding to matcher
+
+# Calling the matcher on the doc & printing the result.
+matches = matcher(doc)
+print([doc[start:end] for match_id, start, end in matches])
