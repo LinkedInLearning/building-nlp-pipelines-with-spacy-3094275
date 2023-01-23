f0bfc3161eb025523bbf0b2f59e1c34882978e33
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-01-23 19:39:46 +0530

    Create 04_ProblemStatement.py

---
+++ 02/04_ProblemStatement.py
@@ -0,0 +1,11 @@
+# Importing Phrase Matcher & initialise it.
+from _________ import _________
+matcher = _________
+
+# Creating pattern doc objects
+patterns = _________
+matcher.add(_________, _________, _________) # Adding to matcher
+
+# Calling the matcher on the doc & printing the result.
+matches = _________
+print(_________)
