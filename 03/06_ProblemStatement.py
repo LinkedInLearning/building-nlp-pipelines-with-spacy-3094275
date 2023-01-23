35bc1c41758c7eabc890d10b32b68a3a2e3abe4d
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-01-23 19:45:18 +0530

    Create 06_ProblemStatement.py

---
+++ 03/06_ProblemStatement.py
@@ -0,0 +1,16 @@
+text_1 = “Linkedin is the best place to expand one’s network.”
+
+nlp = _________
+
+doc = _________(text_1) # Tokenising the text
+
+print(_________)
+
+text_2 = “Linkedin is the best place to expand one’s network.”
+
+# Disable the tagger and parser
+with _________(_________):
+
+  _________ = _________(text_2) # Processing the text
+
+  print(doc._________) # Printing the entities
