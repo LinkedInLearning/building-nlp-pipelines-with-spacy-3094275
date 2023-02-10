9c89857d550aa9b32622b5c587123a216f255a9e
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-02-10 20:16:51 +0530

    Update 03_06b_ProblemStatement.py

--- 03_06b_ProblemStatement.py
+++ 03_06b_ProblemStatement.py
@@ -1,12 +1,18 @@
-text_1 = “Linkedin is the best place to expand one’s network.”
+text_1 = 'Linkedin is the best place to expand one’s network.'
+
+# Tokenising text without using complete NLP pipeline. Thus, printing each token’s corresponding text
 
 nlp = _________
 
 doc = _________(text_1) # Tokenising the text
 
+# Using NLP pipeline for tokenising text without using tagger and parser
+
 print(_________)
 
-text_2 = “Linkedin is the best place to expand one’s network.”
+text_2 = 'Linkedin is the best place to expand one’s network.'
+
+# Printing the entities from the final doc
 
 # Disable the tagger and parser
 with _________(_________):
