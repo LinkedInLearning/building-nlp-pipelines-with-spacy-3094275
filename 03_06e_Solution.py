341f4482ee76e2ece1e3378e9dac50210381da76
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-02-10 20:44:40 +0530

    Update 03_06e_Solution.py

--- 03_06e_Solution.py
+++ 03_06e_Solution.py
@@ -1,12 +1,18 @@
-text_1 = “Linkedin is the best place to expand one’s network.”
+text_1 = 'Linkedin is the best place to expand one’s network.'
 
-nlp = spacy.load(‘en_core_web_sm’)
+# Tokenising text without using complete NLP pipeline. Thus, printing each token’s corresponding text
+
+nlp = spacy.load('en_core_web_sm')
 
 doc = nlp.make_doc(text_1) # Tokenising the text
 
+# Using NLP pipeline for tokenising text without using tagger and parser
+
 print([token.text for token in doc])
 
-text_2 = “Linkedin is the best place to expand one’s network.”
+text_2 = 'Linkedin is the best place to expand one’s network.'
+
+# Printing the entities from the final doc
 
 # Disable the tagger and parser
 with nlp.disable_pipes('tagger', 'parser'):
