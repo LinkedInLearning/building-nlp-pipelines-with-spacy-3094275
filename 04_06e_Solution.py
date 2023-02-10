ee3c95c40c32892217dd9374857b9cb3b31d1731
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-02-10 21:02:26 +0530

    Update 04_06e_Solution.py

--- 04_06e_Solution.py
+++ 04_06e_Solution.py
@@ -1,15 +1,15 @@
 TRAINING_DATA = [
  
-  (“Linkedin is the most powerful place for networking.”, 
+  ('Linkedin is the most powerful place for networking.', 
   
    {'entities': [(0, 8, 'WEBSITE')]}),
  
-  (“Ram has a very attractive profile on Linkedin.”, 
+  ('Ram has a very attractive profile on Linkedin.', 
   
    {'entities': [(0,3, 'PERSON'), (37, 45, 'WEBSITE')]}),
  
-  (“Ram recently published a Machine Learning course on Linkedin Learning.”, 
+  ('Ram recently published a Machine Learning course on Linkedin Learning.', 
   
-   {'entities': [(52, 60, 'WEBSITE'), (0,3, ‘PERSON’)]})
+   {'entities': [(52, 60, 'WEBSITE'), (0,3, 'PERSON')]})
 
 ]
