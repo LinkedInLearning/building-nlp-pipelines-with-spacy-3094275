f81b9b9b1720c5e752b2dd2c4b3cc95691fd3a99
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2023-02-11 07:16:34 +0530

    Update 01_05e_Solution.py

--- 01_05e_Solution.py
+++ 01_05e_Solution.py
@@ -1,31 +1,37 @@
 # Part - 1
 
-text = “Ram goes to college everyday by car.”
+text = 'Ram goes to college everyday by car.'
 
 # Importing spacy and getting all attributes.
 
-nlp = spacy.load(‘en_core_web_sm’)
+nlp = spacy.load('en_core_web_sm')
+
+# Creating doc object
 
 doc = nlp(text)
 
 for token in doc:
+
+  # Getting token’s text, part of speech tags and dependency labels
   
   token_text = token.text # Getting the corresponding text
   
   token_dep = token.dep_ # Getting the Dependency label
   
   token_pos = token.pos_ # Getting the POS Tag
+
+  # Printing entities text and label attributes
   
   print(token_text, token_dep, token_pos)
 
 
 # Part - 2
 
-text = “Ram goes to college everyday by car.”
+text = 'Ram goes to college everyday by car.'
 
 # Importing spacy and getting all attributes.
 
-nlp = spacy.load(‘en_core_web_sm’)
+nlp = spacy.load('en_core_web_sm')
 
 doc = nlp(text)
 
